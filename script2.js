0fc8fe2d2be7b3daa6c5e2de42ec855b6ffd2bc0
Nikolas9896 <nhlynka@gmail.com>
2018-10-04 18:42:03 +0300

    Guess a number :+1:

---
+++ script2.js
@@ -0,0 +1,14 @@
+var userNumber = prompt("Guess a number");
+var hiddenNumber = "10";
+if (userNumber === hiddenNumber) {
+	alert("You WON!!! The number trully 10");
+	console.log("You WON!!! The number trully 10");
+}
+else if (userNumber > hiddenNumber) {
+	alert("Too high. Try again");
+	console.log("Too high. Try again");
+}
+else	{
+	alert("Too low. Try again");
+	console.log("Too low. Try again");
+}
