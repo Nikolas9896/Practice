3f324fa0c226ad8bd78ce1158e194eb9c3412b4c
Nikolas9896 <nhlynka@gmail.com>
2018-10-04 15:27:01 +0300

    Make task in 1 breaf :+1:

---
+++ scriptTask1.js
@@ -0,0 +1,5 @@
+var userFirst = prompt("What is your first name?");
+var userLast = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+alert("You full name is " + userFirst + " " + userLast + " You are " + userAge +" years old");
+console.log("You full name is " + userFirst + " " + userLast + ". You are " + userAge +" years old");
