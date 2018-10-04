c793edb18b9840e904815c27cb04542022d30a92
Nikolas9896 <nhlynka@gmail.com>
2018-10-04 18:43:38 +0300

    Add some text

--- script2.js
+++ script2.js
@@ -1,8 +1,8 @@
 var userNumber = prompt("Guess a number");
 var hiddenNumber = "10";
 if (userNumber === hiddenNumber) {
-	alert("You WON!!! The number trully 10");
-	console.log("You WON!!! The number trully 10");
+	alert("You WON!!! The number is trully 10");
+	console.log("You WON!!! The number is trully 10");
 }
 else if (userNumber > hiddenNumber) {
 	alert("Too high. Try again");
