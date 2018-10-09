b6b699f936e34e8eb8b26482483ce7d65ace2a4a
Nikolas9896 <nhlynka@gmail.com>
2018-10-09 13:59:58 +0300

    Add some variables

--- scriptTask8.js
+++ scriptTask8.js
@@ -1,15 +1,17 @@
 function printReverse(array1)
 {
-	for(var i = (array1.length)-1; i>=0; i--)
+	var last_index=array.length-1;
+	for(var i = last_index; i>=0; i--)
 	{
 		console.log(array1[i]);
 	}
-	console.log("That's all")
 }
+
 function isUniform(array1)
 {
 	var j = true
-	for(var i = 1; i<array1.length && j===true; i++)
+	var length=array.length;
+	for(var i = 1; i<length && j===true; i++)
 	{
 		if(array1[0]!==array1[i])
 		{
@@ -24,7 +26,8 @@ function isUniform(array1)
 function sumArray(array1)
 {
 	var sum=0;
-	for(var i=0; i<array1.length; i++)
+	var length=array.length;
+	for(var i=0; i<length; i++)
 	{
 		
 		sum+=array1[i];
@@ -41,7 +44,8 @@ function sumArray(array1)
 function max(array1)
 {
 	var max=array1[0];
-	for(var i=1; i<array1.length; i++)
+	var length=array.length;
+	for(var i=1; i<length; i++)
 	{
 		if(max<array1[i])
 		{
