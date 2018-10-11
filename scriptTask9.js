1e38118d95fab78e4c5f2aa021e8db0d761f1a4d
Nikolas9896 <nhlynka@gmail.com>
2018-10-11 08:10:11 +0300

    Make the same with "forEach"

--- scriptTask9.js
+++ scriptTask9.js
@@ -1,21 +1,21 @@
 var movieDB = new Array(
 {
-	hasWatched: "watched",
+	hasWatched: true,
 	title: "James Bond 007 Golden Eye",
 	rating: 4.5
 },
 {
-	hasWatched: "not seen",
+	hasWatched: false,
 	title: "Indiana Jones and Last Crusade",
 	rating: 4.3
 },
 {
-	hasWatched: "seen",
+	hasWatched: false,
 	title: "Mad Max Fury Road",
 	rating: 4.8
 },
 {
-	hasWatched: "watched",
+	hasWatched: true,
 	title: "Darkest Hour",
 	rating: 3.2
 }
@@ -24,9 +24,19 @@ var movieDB = new Array(
 
 function showMovieDB()
 {
-	var length=movieDB.length
-	for(var i=0; i<length; i++)
-	{
-		console.log("You have "+movieDB[i].hasWatched+" \""+movieDB[i].title+"\" rating "+movieDB[i].rating +" stars");
-	}
+	movieDB.forEach( function (movie) {
+		var result = "You have ";
+		if (movie.hasWatched)
+			result+="watched ";
+		else result+="not seen ";
+		result+="\""+movie.title+"\" - ";
+		result+= movie.rating+" stars";
+		console.log(result);
+	})
+
+	// var length=movieDB.length
+	// for(var i=0; i<length; i++)
+	// {
+	// 	console.log("You have "+movieDB[i].hasWatched+" \""+movieDB[i].title+"\" rating "+movieDB[i].rating +" stars");
+	// }
 }
